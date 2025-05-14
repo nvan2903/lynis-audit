789563e1869a36524e8066ed47dd08e88610e6de
nvan2903 <tnvan2903@gmail.com>
2025-05-14 15:53:33 +0700

    check flow

--- simulate_risk.sh
+++ simulate_risk.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 echo "Sensitive data" > /tmp/sensitive.conf
 chmod 777 /tmp/sensitive.conf
-useradd -m weakuser
-echo "weakuser:123456" | chpasswd
-echo "Simulated risks: sensitive.conf with 777 permissions and weakuser with weak password"
